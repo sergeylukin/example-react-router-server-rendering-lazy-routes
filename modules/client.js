f7386af82ac8803ae57e7498fdf19021d3e7c937
Morlay <morlay@qq.com>
2016-01-18 14:02:20 +0800

    updated to react-router 2 and babel 6

--- modules/client.js
+++ modules/client.js
@@ -1,18 +1,30 @@
 import React from 'react'
-import { match, Router } from 'react-router'
+import { match, Router, browserHistory } from 'react-router'
 import { render } from 'react-dom'
-import { createHistory } from 'history'
-import routes from './routes/RootRoute'
-
-const { pathname, search, hash } = window.location
-const location = `${pathname}${search}${hash}`
+import RouteRoute from './routes/RootRoute'
 
 // calling `match` is simply for side effects of
 // loading route/component code for the initial location
-match({ routes, location }, () => {
-  render(
-    <Router routes={routes} history={createHistory()} />,
-    document.getElementById('app')
-  )
-})
+const lazyRender = (history, routes) => {
+  const { pathname, search, hash } = window.location
+  const location = `${pathname}${search}${hash}`
 
+  match({
+    routes,
+    location,
+    history
+  }, (error, redirectLocation, renderProps) => {
+    if(redirectLocation) {
+       history.replace(redirectLocation);
+       // redirect and re render
+       lazyRender(history, routes)
+    } else {
+      render(
+        <Router {...renderProps} />,
+        document.getElementById('app')
+      )
+    }
+  })
+}
+
+lazyRender(browserHistory, RouteRoute);
